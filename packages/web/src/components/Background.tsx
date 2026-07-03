4a57777786008d9de900bcb3ef84cb01b90eb6a3
Valentin Dumas <dumas.valentin@imsa.msa.fr>
2026-07-03 11:14:21 +0200

    chore: footer follows branding app name

--- packages/web/src/components/Background.tsx
+++ packages/web/src/components/Background.tsx
@@ -31,7 +31,7 @@ const Background = ({ children }: PropsWithChildren) => {
       >
         <GithubIcon size={14} />
         {/* oxlint-disable-next-line no-undef */}
-        Razzia - v{__APP_VERSION__}
+        {appName} - v{__APP_VERSION__}
       </a>
     </section>
   )
